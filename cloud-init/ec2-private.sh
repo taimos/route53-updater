7bf1fa22bbef136963dc5aa9c8edd3f19cb441b5
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2016-05-14 16:42:25 +0200

    bump version

--- cloud-init/ec2-private.sh
+++ cloud-init/ec2-private.sh
@@ -4,13 +4,13 @@
 #
 # export DNS_HOST=foo
 # export DNS_DOMAIN=example.com
-# curl https://raw.githubusercontent.com/taimos/route53-updater/v1.6/cloud-init/ec2-private.sh | bash
+# curl https://raw.githubusercontent.com/taimos/route53-updater/v1.7/cloud-init/ec2-private.sh | bash
 
 # Route 53 Updater
 
 yum install -y wget
 
-wget -O /root/route53-updater.jar https://github.com/taimos/route53-updater/releases/download/v1.6/route53-updater.jar
+wget -O /root/route53-updater.jar https://github.com/taimos/route53-updater/releases/download/v1.7/route53-updater.jar
 
 java -jar /root/route53-updater.jar --private --domain $DNS_DOMAIN --host $DNS_HOST
 
